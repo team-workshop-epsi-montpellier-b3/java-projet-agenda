fc90dbdef0345639f30376dd827ac0203df77c5b
Max <maxime.egido@gmail.com>
2017-11-10 10:44:18 +0100

    update

--- src/javafxapplication1/JavaFXApplication1.java
+++ src/javafxapplication1/JavaFXApplication1.java
@@ -5,14 +5,20 @@
  */
 package javafxapplication1;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafxapplication1.dao.ConnecteurBdd;
+
 
 /**
  *
@@ -25,25 +31,42 @@ public class JavaFXApplication1 extends Application {
         
         //User user = new User("Guigui", "LANGOUET", "Guillaume");
         //Event event = new Event("4heures de maths", "MR BLABLA, CLASSE 200 Bâtiment 7");
-        ConnecteurBdd aa = new ConnecteurBdd();
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+//        ConnecteurBdd aa = new ConnecteurBdd();
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//            
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//        
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+//        
+//        Scene scene = new Scene(root, 700, 500);
+//        
+//        primaryStage.setTitle("Hello World!");
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+
+Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/views/FXMLDocument.fxml"));
+            Scene scene = new Scene(root);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+       
+//        DatabaseConnection.creerTables();
         
-        Scene scene = new Scene(root, 700, 500);
-        
-        primaryStage.setTitle("Hello World!");
+                
+                
         primaryStage.setScene(scene);
         primaryStage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(JavaFXApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 
     /**
